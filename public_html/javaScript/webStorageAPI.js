63a46f0aec03ed7eab516cf1451fc2be39fdb5ce
Marlon <m.c4n1as@gmail.com>
2018-02-15 01:47:35 -0700

    added some styles and javascript code

--- public_html/javaScript/webStorageAPI.js
+++ public_html/javaScript/webStorageAPI.js
@@ -4,4 +4,19 @@
  * and open the template in the editor.
  */
 
+function doFirst(){
+    var button = document.getElementById("button");
+    button.addEventListener("click",saveStuff,false);
+}
 
+function saveStuff(){
+    var one = document.getElementById("one").value;
+    var two = document.getElementById("two").value;
+    
+    //built in function that saves. It takes two parameters
+    //key parmeter is the name of the variable. Then value variable is 
+    //the value of the key.
+    sessionStorage.setItem(one,two);
+}
+
+window.addEventListener("load",doFirst,false);
