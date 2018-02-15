76ed2a22ad78bed55caf1131cf7db126e9827b71
Marlon <m.c4n1as@gmail.com>
2018-02-15 01:57:32 -0700

    Part 3
    Created a display function to show the name and value of the variable in the blue box

--- public_html/javaScript/webStorageAPI.js
+++ public_html/javaScript/webStorageAPI.js
@@ -13,10 +13,18 @@ function saveStuff(){
     var one = document.getElementById("one").value;
     var two = document.getElementById("two").value;
     
-    //built in function that saves. It takes two parameters
+    //built in function setItem in sessionStorage that saves. It takes two parameters
     //key parmeter is the name of the variable. Then value variable is 
     //the value of the key.
     sessionStorage.setItem(one,two);
+    
+    display(one);
+}
+
+function display(one){
+    var rightBox = document.getElementById("rightbox");
+    var two = sessionStorage.getItem(one);
+    rightBox.innerHTML = "Name of variable: "+one+"<br /> Value: "+two;
 }
 
 window.addEventListener("load",doFirst,false);
